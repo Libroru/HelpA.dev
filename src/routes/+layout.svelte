10241fb56dde2382821e3976ab8f7d61e720104b
Alexander Klann <alex.klann16@gmail.com>
2023-07-01 13:49:00 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script>
+	import '../app.scss';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<nav class="navbar navbar-light bg-light p-2">
+		<a class="navbar-brand" href="/">HelpA.dev</a>
+		<form class="d-inline-flex">
+			<input class="form-control mx-2" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success" type="submit">Search</button>
+		</form>
+	</nav>
+
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<section class="d-flex flex-row justify-content-center align-items-center">
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group my-2">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</section>
